706b106ee9a415342c60c97b5888ada71f992d79
gazbuntu <gaz@gazaxian.com>
2014-01-08 15:43:24 +0000

    Moved some rand functions into own file

---
+++ src/gaz/rand.clj
@@ -0,0 +1,13 @@
+(ns gaz.rand
+  (:require
+    [gaz.math2 :as math]))
+
+(defn rnd-rng [lo hi] (+ lo (rand (- hi lo))))
+
+(defn rnd-norm [] (rnd-rng -0.5 0.5))
+
+(defn rnd-vec [vmin vmax kyz]
+  (map #(rnd-rng (vmin %1) (vmax %1)) kyz ))
+
+(defn rnd-v3 [mn mx]
+  (apply math/mk-vec (rnd-vec mn mx [0 1 2])))
